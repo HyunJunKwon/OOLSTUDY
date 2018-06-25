546ceb6cdad45c67676a8f2491a3dc4903534930
Francesc Kwon <HyunJunKwon@users.noreply.github.com>
2018-06-25 15:51:04 +0900

    Update 3.StaticMember.cpp

--- CPP/1.Friend Static Const/3.StaticMember.cpp
+++ CPP/1.Friend Static Const/3.StaticMember.cpp
@@ -1,6 +1,13 @@
 /*
-클래스 내에 선언된 const 멤버변수(상수)의 초기화는 이니셜라이저를 통해야만한다.
-앞선 예제와 같이,
+static 개념.
+전역변수에 선언된 static의 의미
+-> 선언된 파일 내에서만 참조를 허용하겠다는 의미
+함수 내에 선언된 static의 의미
+-> 한번만 초기화되고, 지역변수와 달리 함수를 빠져나가도 소멸되지 않는다.
+*/
+#include <iostream>
+using namespace std;
+
 class SoSimple
 {
 private:
@@ -12,30 +19,34 @@ public:
 		cout << simObjCnt << "th SoSimple object" << endl;
 	}
 };
-int SoSimple::simObjCnt = 0;
+int SoSimple::simObjCnt = 0; // 메모리 공간에 할당하기 때문에 별도로 선언!!!!  
 
-
-그러나 const static으로 선언되는 멤버변수(상수)는 다음과 같이 선언과 동시에 초기화가 가능하다.
-const static 멤버변수는, 클래스가 정의될 때 지정된 값이 유지되는 상수이기 때문에, 초기화가 가능!!!!
-*/
-
-#include <iostream>
-using namespace std;
-
-class CountryArea
+class SoComplex
 {
+private:
+	static int cmxObjCnt;
 public:
-	const static int RUSSIA = 1707540;
-	const static int CANADA = 998467;
-	const static int CHINA = 957290;
-	const static int SOUTH_KOREA = 9922;
+	SoComplex()
+	{
+		// simObjCnt = 0;  <- 틀린문장!!!! 이미 할당이루어졌기 때문,. 
+		cmxObjCnt++;
+		cout << cmxObjCnt << "th SoComplex object" << endl;
+	}
+	SoComplex(SoComplex &copy)
+	{
+		cmxObjCnt++;
+		cout << cmxObjCnt << "th SoComplex object" << endl;
+	}
 };
+int SoComplex::cmxObjCnt = 0;
 
 int main(void)
 {
-	cout << "러시아 면적: " << CountryArea::RUSSIA << "㎢" << endl;
-	cout << "캐나다 면적: " << CountryArea::CANADA << "㎢" << endl;
-	cout << "중국 면적: " << CountryArea::CHINA << "㎢" << endl;
-	cout << "한국 면적: " << CountryArea::SOUTH_KOREA << "㎢" << endl; // 정의된 상수에 접근하기 위해서 굳이 객체를 생성할 필요는 없다..  
+	SoSimple sim1; // 초기화가 되면서 cout 출력
+	SoSimple sim2;
+
+	SoComplex cmx1;
+	SoComplex cmx2 = cmx1;
+	SoComplex();
 	return 0;
 }
