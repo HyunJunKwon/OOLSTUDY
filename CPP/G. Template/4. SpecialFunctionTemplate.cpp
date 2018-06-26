4dd25cd2c7384fcc271b235ea0c9f189668c3c95
Francesc Kwon <HyunJunKwon@users.noreply.github.com>
2018-06-26 18:59:28 +0900

    Update 4. SpecialFunctionTemplate.cpp

--- CPP/G. Template/4. SpecialFunctionTemplate.cpp
+++ CPP/G. Template/4. SpecialFunctionTemplate.cpp
@@ -7,22 +7,18 @@ T Max(T a, T b)
 {
 	return a > b ? a : b;
 }
-
 template <>
 char* Max(char* a, char* b)
 {
 	cout << "char* Max<char*>(char* a, char* b)" << endl;
 	return strlen(a) > strlen(b) ? a : b;
 }
-
 template <>
 const char* Max(const char* a, const char* b)
 {
 	cout << "const char* Max<const char*>(const char* a, const char* b)" << endl;
 	return strcmp(a, b) > 0 ? a : b;
 }
-
-
 int main(void)
 {
 	cout << Max(11, 15) << endl;
