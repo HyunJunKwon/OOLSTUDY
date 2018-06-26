9f9331293bf8f04aa81afcc80ff3504a4ed1fff9
Francesc Kwon <HyunJunKwon@users.noreply.github.com>
2018-06-26 14:34:48 +0900

    Update 1. FunctionVirtualOverride.cpp

--- CPP/C. Inheritance Polymorphism/1. FunctionVirtualOverride.cpp
+++ CPP/C. Inheritance Polymorphism/1. FunctionVirtualOverride.cpp
@@ -1,6 +1,6 @@
 /*
-C++에서, AAA형 포인터 변수는
-AAA 객체 또는 AAA를 직접 혹은 간접적으로 상속하는
+C++에서, BBB형 포인터 변수는 class BBB : public AAA 일때,
+BBB 객체 또는 BBB를 직접 혹은 간접적으로 상속하는 (AAA)
 모든 객체를 가리킬 수 있다.(객체의 주소 값을 저장할 수 있다)
 int main(void)
 {
@@ -58,16 +58,17 @@ int main(void)
 	Third * tptr = new Third(); // 생성
 	Second * sptr = tptr;
 	First * fptr = sptr;
-	// (F + S + T)를 가진 tptr을 (S + T)가진 sptr에 할당,
-	// 또한 이를 fptr에 할당. 결론적으로 (F + S + T)를 가짐.
-	// 밑의 코드를 실행하면, 제일 마지막으로 오버라이딩된 tptr의 MyFunc()가 실행
+	// ThirdFunc를 가진 tptr을 sptr에 할당, (tptr은 sptr을 상속함으로 가능)
+	// 또한 이를 fptr에 할당,
+	// 밑의 코드를 실행하면, 제일 마지막으로 tptr의 가상함수 MyFunc()가 실행
 	fptr->MyFunc();
 	sptr->MyFunc();
 	tptr->MyFunc();
-	/* output:
-	third
-	third
-	third
+	/* 
+	output:
+		ThirdFunc
+		ThirdFunc
+		ThirdFunc
 	*/
 	delete tptr;
 	return 0;
